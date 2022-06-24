d9757eddba9d099e147978a16a04c1abc8cdb3b8
Patrick Hladun <patrickhladun@gmail.com>
2022-06-24 14:13:58 +0100

    Add about page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,24 +9,34 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", {
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-mdx", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-image", 
+    "gatsby-plugin-react-helmet",
+  {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
       "trackingId": "00000000000"
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", {
+  }, 
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
+  }, 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
---
+++ src/pages/about.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+// markup
+const AboutPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>About Page</title>
+      <h1 style={headingStyles}>About</h1>
+    </main>
+  )
+}
+
+export default AboutPage
